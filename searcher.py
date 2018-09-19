f1d5983be9998ac0a18a2bdd39ae0bcdfce1c0d2
Fares Alarabi <f.alarabi@darkcoders.com>
2018-09-19 05:27:34 -0400

    Bulding the search engine

---
+++ searcher.py
@@ -0,0 +1,12 @@
+import csv
+import sys
+
+#input number you want to search
+number = input('Enter number to find\n')
+
+
+with open('nessus.csv') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		if number == row['Plugin']:
+			print(row)
