65ac1e2da3d33a41412a5ed8266eebb427f600b7
Fares Alarabi <42983245+0xfares@users.noreply.github.com>
2018-11-25 23:58:24 +0300

    been updated

--- all.py
+++ all.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-
+from collections import defaultdict
 
 #Defining list of variables hold the actual data.
 #IP Addresses Varible
@@ -16,8 +16,8 @@ RowOfDataFamily = list()
 RowOfDataSeverity = []
 RowOfDataSeverity = list()
 #IP Address Varible
-RowOfDataIPAddress = []
-RowOfDataIPAddress = list()
+RowOfDataHostName = []
+RowOfDataHostName = list()
 #Protocol Varible
 RowOfDataProtocol = []
 RowOfDataProtocol = list()
@@ -85,53 +85,81 @@ FullData = list()
 with open('nessus.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		PluginRow = row['Plugin']
+		PluginRow = row['Plugin ID']
 		SingleRow.append(PluginRow)
 #print(len(AllData))
 #print(AllData[0])
 
 
-print("\n")
-print("********************************************************************************************************")
-print("						Printing the whole data...				       ")
-print("********************************************************************************************************")
-print(SingleRow)
-print("========================================================================================================")
-print("\n")
-print("********************************************************************************************************")
-print("						Sorting the data...					       ")
-print("********************************************************************************************************")
+#print("\n")
+#print("********************************************************************************************************")
+#print("						Printing the whole data...				       ")
+#print("********************************************************************************************************")
+#print(SingleRow)
+#print("========================================================================================================")
+#print("\n")
+#print("********************************************************************************************************")
+#print("						Sorting the data...					       ")
+#print("********************************************************************************************************")
 SingleRow.sort()
-print(SingleRow)
-print("========================================================================================================")
-print("\n")
-print("********************************************************************************************************")
-print("						Removing the dublicates...				       ")
-print("********************************************************************************************************")
+#print(SingleRow)
+#print("========================================================================================================")
+#print("\n")
+#print("********************************************************************************************************")
+#print("						Removing the dublicates...				       ")
+#print("********************************************************************************************************")
 RemovingDublicates1 = list(set(SingleRow))
 RemovingDublicates1.sort()
-print(RemovingDublicates1)
-print("========================================================================================================")
-print("\n")
+#print(RemovingDublicates1)
+#print("========================================================================================================")
+#print("\n")
 i = 0
 while i < len(RemovingDublicates1):
 	with open('nessus.csv') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-				if RemovingDublicates1[i] == row['Plugin']:
+				if RemovingDublicates1[i] == row['Plugin ID']:
 					#fetching the data as rows
-					PluginNameRow = row['Plugin Name']
-		
+					PluginNameRow = row['Name']
+					HostNameRow = row['Host']
+					ProtocolRow = row['Protocol']
+					PortRow = row['Port']
+
 					#Appending the results in the lists
 					RowOfDataPluginName.append(PluginNameRow)
-
+					RowOfDataHostName.append(HostNameRow)
+					RowOfDataProtocol.append(ProtocolRow)
+					RowOfDataPort.append(PortRow)
 		
-					AllData= list(set(RowOfDataPluginName))
-	#AllData.append({'Plugin Name':RemovingDuplicatesFromPluginName})
+					#RowOfDataPluginName.sort()
+					#RowOfDataHostName.sort()
+
+					AllData = list(RowOfDataPluginName)
+					AllData1 = list(RowOfDataHostName)
+					AllData2 = list(RowOfDataProtocol)
+					AllData3 = list(RowOfDataPort)
 		i += 1	
-	#print(RemovingDublicates[i])
-print(AllData)
+#print(AllData)
+#print(len(AllData))
+#print(len(AllData1))
+AllDataMeans = []
+AllDataMeans = list()
+for j in range(len(AllData)):
+	print(AllData[j])
+	matches = [index for index, value in enumerate(AllData) if value == AllData[j]]
+	#display the data based on matches list hostnames,an more
+	for xx in range(len(matches)): 
+		print(AllData1[xx],AllData2[xx],AllData3[xx])
+	print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+	AllDataMeans.append(({AllData[j]:[AllData1[j],AllData2[j],AllData3[j]]}))
+matches = [index for index, value in enumerate(AllData) if value == "SSL Certificate Signed Using Weak Hashing Algorithm (Known CA)" ]
+print(matches)
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+#res = defaultdict(list)
+#for vulName,vulHost in AllDataMeans: 
+#	res[vulName].append(vulHost)
+#print([{'vulnerablity':vulName, 'host':vulHost} for vulName,vulHost in res.items()])
                 #PluginRow = row['Plugin']
                 #AllData.append(PluginRow)
 	#print(x)
